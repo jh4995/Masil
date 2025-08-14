4a49970e359f81ee1ec0585cd9a2c044ac750037
PLMQ2785 <dnjsdlf325@gmail.com>
2025-08-14 15:50:40 +0900

    fix

--- masilProject/user-web-app/src/App.jsx
+++ masilProject/user-web-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { supabase } from './supabaseClient';
 import HomePage from './pages/HomePage';
 import AdminPage from './pages/AdminPage';
